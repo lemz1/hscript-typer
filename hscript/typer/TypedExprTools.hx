31bad27a1b03907be21cb2954b62b5c196a3b7f9
lemz1 <ismael.amjad07@gmail.com>
2025-07-09 00:52:44 +0200

    made tools code more readable and better

--- hscript/typer/TypedExprTools.hx
+++ hscript/typer/TypedExprTools.hx
@@ -5,75 +5,119 @@ import hscript.Printer;
 
 class TypedExprTools
 {
-  // TODO: add tabs + make code more readable + fix some stuff
+  static final TAB:String = '  ';
+
   public static function toString(e:TypedExpr, printTypes:Bool):String
   {
-    var str:String = switch (e.e)
+    var str:String = '';
+    switch (e.e)
     {
       case TEConst(c):
         switch (c)
         {
           case CInt(v):
-            '${v}';
+            str += '${v}';
           case CFloat(f):
-            '${f}';
+            str += '${f}';
           case CString(s):
-            s;
+            str += s;
         }
       case TEIdent(v):
-        v;
+        str += v;
       case TEVar(n, t, e1):
-        'var ${n}${t != null ? ':${typeToString(t)}' : ''}${e1 != null ? ' = ${toString(e1, printTypes)}' : ''}';
+        str += 'var ${n}';
+        if (t != null) str += ':${typeToString(t)}';
+        if (e1 != null) str += ' = ${toString(e1, printTypes)}';
       case TEParent(e1):
-        '(${toString(e1, printTypes)})';
+        str += '(${toString(e1, printTypes)})';
       case TEBlock(es):
-        es.length > 0 ? '{\n${[for (e in es) toString(e, printTypes)].join(';\n')}\n}' : '{}';
+        if (es.length > 0)
+        {
+          str += '{\n';
+          str += applyTab([for (e in es) '${toString(e, printTypes)};'].join('\n'));
+          str += '\n}';
+        }
+        else
+        {
+          str += '{}';
+        }
       case TEField(e1, f):
-        '${toString(e1, printTypes)}.${f}';
+        str += '${toString(e1, printTypes)}.${f}';
       case TEBinop(op, e1, e2):
-        '${toString(e1, printTypes)} ${op} ${toString(e2, printTypes)}';
+        str += '${toString(e1, printTypes)} ${op} ${toString(e2, printTypes)}';
       case TEUnop(op, prefix, e1):
-        '${prefix ? op : ''}${toString(e1, printTypes)}${!prefix ? op : ''}';
+        if (prefix) str += op;
+        str += toString(e1, printTypes);
+        if (!prefix) str += op;
       case TECall(e1, params):
-        '${toString(e1, printTypes)}(${[for (p in params) toString(p, printTypes)].join(', ')})';
+        str += toString(e1, printTypes);
+        str += '(';
+        str += [for (p in params) toString(p, printTypes)].join(', ');
+        str += ')';
       case TEIf(cond, e1, e2):
-        'if (${toString(cond, printTypes)}) ${toString(e1, printTypes)}${e2 != null ? ' else ${toString(e2, printTypes)}' : ''}';
+        str += 'if (${toString(cond, printTypes)})';
+        str += toString(e1, printTypes);
+        if (e2 != null) str += ' else ${toString(e2, printTypes)}';
       case TEWhile(cond, e1):
-        'while (${toString(cond, printTypes)}) ${toString(e1, printTypes)}';
+        str += 'while (${toString(cond, printTypes)}) ${toString(e1, printTypes)}';
       case TEFor(v, it, e1):
-        'for (${v} in ${toString(it, printTypes)}) ${toString(e1, printTypes)}';
+        str += 'for (${v} in ${toString(it, printTypes)}) ${toString(e1, printTypes)}';
       case TEBreak:
-        'break';
+        str += 'break';
       case TEContinue:
-        'continue';
+        str += 'continue';
       case TEFunction(args, e1, name, ret):
-        'function${name != null ? ' ${name}' : ''}(${[for (a in args) '${a.opt != null && a.opt ? '?' : ''}${a.name}:${typeToString(a.t)}${a.value != null ? ' = ${toString(a.value, printTypes)}' : ''}'].join(', ')})${ret != null ? ':${typeToString(ret)}' : ''} ${toString(e1, printTypes)}';
+        str += 'function';
+        if (name != null) str += ' ${name}';
+        str += '(';
+        for (i => a in args)
+        {
+          if (a.opt != null && a.opt) str += '?';
+          str += '${name}:${typeToString(a.t)}';
+          if (a.value != null) str += ' = ${toString(a.value, printTypes)}';
+          if (i != args.length - 1) str += ', ';
+        }
+        str += ')';
+        if (ret != null) str += ':${typeToString(ret)}';
+        str += ' ${toString(e1, printTypes)}';
       case TEReturn(e1):
-        'return${e1 != null ? ' ${toString(e1, printTypes)}' : ''}';
+        str += 'return';
+        if (e1 != null) str += ' ${toString(e1, printTypes)}';
       case TEArray(e1, index):
-        '${toString(e1, printTypes)}[${toString(index, printTypes)}]';
+        str += '${toString(e1, printTypes)}[${toString(index, printTypes)}]';
       case TEArrayDecl(es):
-        '[${[for (e in es) toString(e, printTypes)].join(', ')}]';
+        str += '[${[for (e in es) toString(e, printTypes)].join(', ')}]';
       case TENew(cl, params):
-        'new ${cl}(${[for (p in params) toString(p, printTypes)].join(', ')})';
+        str += 'new ${cl}(${[for (p in params) toString(p, printTypes)].join(', ')})';
       case TEThrow(e1):
-        'throw ${toString(e1, printTypes)}';
+        str += 'throw ${toString(e1, printTypes)}';
       case TETry(e1, v, t, ecatch):
-        'try ${toString(e1, printTypes)} catch(${v}${t != null ? ':${typeToString(t)}' : ''}) ${toString(ecatch, printTypes)}';
+        str += 'try ${toString(e1, printTypes)} ';
+        str += 'catch(${v}';
+        if (t != null) str += ':${typeToString(t)}';
+        str += ') ${toString(ecatch, printTypes)}';
       case TEObject(fl):
-        '{\n${[for (f in fl) '${f.name}: ${toString(f.e, printTypes)}'].join(',\n')}\n}';
+        str += '{\n${[for (f in fl) '${f.name}: ${toString(f.e, printTypes)}'].join(',\n')}\n}';
       case TETernary(cond, e1, e2):
-        '${toString(cond, printTypes)} ? ${toString(e1, printTypes)} : ${toString(e2, printTypes)}';
+        str += '${toString(cond, printTypes)} ? ${toString(e1, printTypes)} : ${toString(e2, printTypes)}';
       case TESwitch(e1, cases, defaultExpr):
-        'switch (${toString(e1, printTypes)}) {\n${[for (c in cases) 'case ${[for (v in c.values) toString(v, printTypes)].join(' | ')}: ${toString(c.expr, printTypes)}'].join('\n')}${defaultExpr != null ? '\ndefault: ${toString(defaultExpr, printTypes)}' : ''}\n}';
+        str += 'switch (${toString(e1, printTypes)}) {\n';
+        str += [
+          for (c in cases)
+            'case ${[for (v in c.values) toString(v, printTypes)].join(' | ')}: ${toString(c.expr, printTypes)}'
+        ].join('\n');
+        if (defaultExpr != null) str += '\ndefault: ${toString(defaultExpr, printTypes)}';
+        str += '\n}';
       case TEDoWhile(cond, e1):
-        'do ${toString(e1, printTypes)} while (${toString(cond, printTypes)})';
+        str += 'do ${toString(e1, printTypes)} while (${toString(cond, printTypes)})';
       case TEMeta(name, args, e1):
-        '@${name}${args.length > 0 ? '(${[for (a in args) toString(a, printTypes)].join(', ')})' : ''} ${toString(e1, printTypes)}';
+        str += '@${name}';
+        if (args.length > 0) str += [for (a in args) toString(a, printTypes)].join(', ');
+        str += ' ${toString(e1, printTypes)}';
       case TECheckType(e1, t):
-        '${toString(e1, printTypes)}:${typeToString(t)}';
+        str += '${toString(e1, printTypes)}:${typeToString(t)}';
       case TEForGen(it, e1):
-        'for (${toString(it, printTypes)}) ${toString(e1, printTypes)}';
+        str += 'for (${toString(it, printTypes)}) ${toString(e1, printTypes)}';
       default:
         return '<invalid>';
     }
@@ -83,5 +127,10 @@ class TypedExprTools
   static function typeToString(t:CType):String
   {
     return new Printer().typeToString(t);
+  }
+
+  static function applyTab(s:String):String
+  {
+    return [for (l in s.split('\n')) '${TAB}${l}'].join('\n');
   }
 }
